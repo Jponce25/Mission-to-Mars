6592adb940b5ebfea0c9d807391c8d538ec8ccaf
Jorge Eduardo Ponce Leyva <jorge.ponce@ift.org.mx>
2021-11-28 20:12:46 -0600

    changes to line 8 to store it in mongodb

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://jorge:ponce@127.0.0.1:27017/mars_app"
+# Normal Code
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+# However, on my computer for security reasons I require the following code to run including the User and Pass of the MongoDB
+# app.config["MONGO_URI"] = "mongodb://user:pass@127.0.0.1:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
